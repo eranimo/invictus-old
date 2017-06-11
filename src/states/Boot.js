8658e66d0d7ab4d56fffc12ac5e5a9376dc94029
Kaelan Cooter <dcgamers@gmail.com>
2017-06-10 20:36:15 -0400

    initial commit

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,11 +1,11 @@
-import Phaser from 'phaser'
-import WebFont from 'webfontloader'
+import Phaser from 'phaser';
+import WebFont from 'webfontloader';
 
 export default class extends Phaser.State {
   init () {
-    this.stage.backgroundColor = '#EDEEC9'
-    this.fontsReady = false
-    this.fontsLoaded = this.fontsLoaded.bind(this)
+    this.stage.backgroundColor = '#2d2d2d';
+    this.fontsReady = false;
+    this.fontsLoaded = this.fontsLoaded.bind(this);
   }
 
   preload () {
@@ -14,22 +14,22 @@ export default class extends Phaser.State {
         families: ['Bangers']
       },
       active: this.fontsLoaded
-    })
+    });
 
-    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
-    text.anchor.setTo(0.5, 0.5)
+    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' });
+    text.anchor.setTo(0.5, 0.5);
 
-    this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
+    this.load.image('loaderBg', './assets/images/loader-bg.png');
+    this.load.image('loaderBar', './assets/images/loader-bar.png');
   }
 
   render () {
     if (this.fontsReady) {
-      this.state.start('Splash')
+      this.state.start('Splash');
     }
   }
 
   fontsLoaded () {
-    this.fontsReady = true
+    this.fontsReady = true;
   }
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -1,22 +1,23 @@
-import Phaser from 'phaser'
-import { centerGameObjects } from '../utils'
+import Phaser from 'phaser';
+import { centerGameObjects } from '../utils';
 
 export default class extends Phaser.State {
   init () {}
 
   preload () {
-    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-    centerGameObjects([this.loaderBg, this.loaderBar])
+    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg');
+    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar');
+    centerGameObjects([this.loaderBg, this.loaderBar]);
 
-    this.load.setPreloadSprite(this.loaderBar)
+    this.load.setPreloadSprite(this.loaderBar);
     //
     // load your assets
     //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
+    this.load.image('mushroom', 'assets/images/mushroom2.png');
+    this.load.image('terrain', 'assets/images/terrain.png');
   }
 
   create () {
-    this.state.start('Game')
+    this.state.start('Game');
   }
 }
