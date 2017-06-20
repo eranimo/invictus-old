841404bf306d3c5a5eb568e1d0b3d918eeb8cf60
Kaelan Cooter <dcgamers@gmail.com>
2017-06-19 23:52:18 -0400

    perfect zoomable noise with two levels

--- src/states/Map.js
+++ src/states/Map.js
@@ -4,55 +4,83 @@ import Alea from 'alea';
 import { time } from 'core-decorators';
 
 
+const zoomableNoise = noise => (numIterations, nx, ny, persistence, scale) => {
+  // persistence is the scale factor in each iteration
+  let maxAmp = 0;
+  let amp = 1; // relative importance of the octave in the sum of the octaves
+  let freq = scale;
+  let noiseValue = 0;
+  const low = 0;
+  const high = 255;
+
+  // add successively smaller, higher-frequency terms
+  for (let i = 0; i < numIterations; ++i) {
+    noiseValue += noise(nx * freq, ny * freq) * amp;
+    maxAmp += amp;
+    amp *= persistence;
+    freq *= 2;
+  }
+
+  // take the average value of the iterations
+  noiseValue /= maxAmp;
+
+  // normalize the result
+  noiseValue = noiseValue * (high - low) / 2 + (high + low) / 2;
+
+  return noiseValue;
+};
+
+const contour = (value, c = 10) => Math.ceil(value / c) * c;
+const coastline = value => value < 200 ? 0 : 255;
+
+
 export default class Map extends Phaser.State {
   init() {
     this.stage.backgroundColor = '#2d2d2d';
-    this.size = 300;
+    this.size = 250;
     this.regionScale = 10;
     this.regionSize = this.size / this.regionScale;
+    this.seed = Math.random();
+    this.activeRegion = {
+      x: 0,
+      y: 0,
+    };
   }
 
   @time
-  generateMap() {
-    const rng = new Alea();
+  makeMap() {
+    const rng = new Alea(this.seed);
     const simplex = new SimplexNoise(rng);
     const noise = (nx, ny) => simplex.noise2D(nx, ny) / 2 + 0.5;
-
-    const worldMapHeight = (nx, ny) => {
-      const e = 1.00 * noise(nx * 1, ny * 1)
-              + 0.80 * noise(nx * 2, ny * 2)
-              + 0.20 * noise(nx * 6, ny * 6);
-      return e;
-    };
 
     for (let x = 0; x < this.size; x++) {
       for (let y = 0; y < this.size; y++) {
         const nx = x / this.size - 0.5;
         const ny = y / this.size - 0.5;
-        let height = ((worldMapHeight(nx, ny) + 0.5) / 3.5) * 256;
-        height = Math.min(Math.max(height, 0), 255);
-        height = Math.ceil(height / 10) * 10;
+        let height = zoomableNoise(noise)(5, nx, ny, .5, 4);
+        height = contour(height);
         this.worldMapData.setPixel(x, y, height, height, height, false);
       }
     }
 
     this.worldMapData.context.putImageData(this.worldMapData.imageData, 0, 0);
     this.worldMapData.dirty = true;
+  }
 
+  @time
+  makeRegion() {
+    const rng = new Alea(this.seed);
+    const simplex = new SimplexNoise(rng);
+    const noise = (nx, ny) => simplex.noise2D(nx, ny) / 2 + 0.5;
     // region map
-    const regionMapHeight = (nx, ny) => {
-      return (0.5 * noise(nx * 20, ny * 20));
-    };
-    const offsetX = this.regionSize * 0;
-    const offsetY = this.regionSize * 0;
+    const offsetX = this.size * this.activeRegion.x;
+    const offsetY = this.size * this.activeRegion.y;
     for (let x = 0; x < this.regionSize * this.regionScale; x++) {
       for (let y = 0; y < this.regionSize * this.regionScale; y++) {
         const nx = ((x + offsetX) / this.regionScale) / this.size - 0.5;
         const ny = ((y + offsetY) / this.regionScale) / this.size - 0.5;
-        const e = worldMapHeight(nx, ny) + regionMapHeight(nx, ny);
-        let height = (e / 3.5) * 256;
-        height = Math.min(Math.max(height, 0), 255);
-        height = Math.ceil(height / 10) * 10;
+        let height = zoomableNoise(noise)(25, nx, ny, .51, 4);
+        height = contour(height, 5);
         this.regionMapData.setPixel(x, y, height, height, height, false);
       }
     }
@@ -72,7 +100,29 @@ export default class Map extends Phaser.State {
         gridMap.line(0, y * cellSize, width, y * cellSize, '#000');
       }
     }
-    this.game.add.sprite(mask.left, mask.top, gridMap);
+    const gridSprite = this.game.add.sprite(mask.left, mask.top, gridMap);
+    gridSprite.smoothed = false;
+    return gridSprite;
+  }
+
+  makeRegionCursor() {
+    const cursorMap = this.game.add.bitmapData(this.size, this.size);
+    cursorMap.context.strokeStyle = '#FF0000';
+    cursorMap.context.rect(
+      1,
+      1,
+      (this.regionSize * 2) - 2,
+      (this.regionSize * 2) - 2,
+    );
+    cursorMap.context.stroke();
+    this.worldMapCursor = this.game.add.sprite(this.size, this.size, cursorMap);
+    this.worldMapCursor.smoothed = false;
+    this.updateRegionCursor();
+  }
+
+  updateRegionCursor() {
+    this.worldMapCursor.left = 10 + this.activeRegion.x * this.regionSize * 2;
+    this.worldMapCursor.top = 10 + this.activeRegion.y * this.regionSize * 2;
   }
 
   create() {
@@ -85,20 +135,38 @@ export default class Map extends Phaser.State {
 
     this.worldMapData = this.game.add.bitmapData(this.size, this.size);
     this.regionMapData = this.game.add.bitmapData(this.regionSize * this.regionScale, this.regionSize * this.regionScale);
-    this.generateMap();
+    this.makeMap();
+    this.makeRegion();
 
-    this.worldMap = this.game.add.sprite(100, 100, this.worldMapData);
+    this.worldMap = this.game.add.sprite(10, 10, this.worldMapData);
     this.worldMap.smoothed = false;
     this.worldMap.scale.set(2);
 
-    this.makeGrid(this.worldMap, this.regionScale);
+    const grid = this.makeGrid(this.worldMap, this.regionScale, (x, y) => {
+      return this.activeRegion.x === x && this.activeRegion.y === y;
+    });
+    grid.events.onInputDown.add(() => {
+      const input = grid.input;
+      const cx = Math.floor(input.pointerX() / (this.regionSize * 2));
+      const cy = Math.floor(input.pointerY() / (this.regionSize * 2));
+      this.activeRegion.x = cx;
+      this.activeRegion.y = cy;
+      this.updateRegionCursor();
+      this.makeRegion();
+    });
+    grid.inputEnabled = true;
+    grid.pixelPerfectClick = true;
+    grid.useHandCursor = true;
 
-    this.regionMap = this.game.add.sprite(100 + this.worldMap.width, 100, this.regionMapData);
+    this.regionMap = this.game.add.sprite(10 + this.worldMap.width, 10, this.regionMapData);
     this.regionMap.smoothed = false;
     this.regionMap.scale.set(2);
 
     refreshKey.onUp.add(() => {
-      this.generateMap();
+      this.seed = Math.random();
+      this.makeMap();
     });
+
+    this.makeRegionCursor();
   }
 }
